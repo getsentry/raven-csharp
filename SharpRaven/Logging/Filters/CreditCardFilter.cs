d6fca5e9349e9c73dbe07593de708e0d2d964654
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-09-27 12:52:08 +0200

    Local variables should be lowercase. Renamed DELTAS to deltas.

--- SharpRaven/Logging/Filters/CreditCardFilter.cs
+++ SharpRaven/Logging/Filters/CreditCardFilter.cs
@@ -28,14 +28,14 @@ namespace SharpRaven.Logging.Filters {
             number.Replace("-", String.Empty);
             number.Replace(" ", String.Empty);
 
-            int[] DELTAS = new int[] { 0, 1, 2, 3, 4, -4, -3, -2, -1, 0 };
+            int[] deltas = new int[] { 0, 1, 2, 3, 4, -4, -3, -2, -1, 0 };
             int checksum = 0;
             char[] chars = number.ToCharArray();
             for (int i = chars.Length - 1; i > -1; i--) {
                 int j = ((int)chars[i]) - 48;
                 checksum += j;
                 if (((i - chars.Length) % 2) == 0)
-                    checksum += DELTAS[j];
+                    checksum += deltas[j];
             }
 
             return ((checksum % 10) == 0);
