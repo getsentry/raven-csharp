033cb06a81051da91730d93f339b988c402d3ce7
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-10-15 16:12:31 +0200

    Added documentation to SentryUser

--- SharpRaven/Data/SentryUser.cs
+++ SharpRaven/Data/SentryUser.cs
@@ -4,8 +4,16 @@ using Newtonsoft.Json;
 
 namespace SharpRaven.Data
 {
+    /// <summary>
+    /// An interface which describes the authenticated User for a request.
+    /// You should provide at least either an id (a unique identifier for an authenticated user) or ip_address (their IP address).
+    /// </summary>
     public class SentryUser
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SentryUser"/> class.
+        /// </summary>
+        /// <param name="principal">The principal.</param>
         public SentryUser(IPrincipal principal)
         {
             if (principal == null)
