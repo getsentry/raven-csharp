87e76fcfb2e1149b74dfaf2c03ca2ad223608364
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-10-15 15:51:28 +0200

    Added properties to SentryUser

--- SharpRaven/Data/SentryUser.cs
+++ SharpRaven/Data/SentryUser.cs
@@ -1,7 +1,19 @@
-﻿namespace SharpRaven.Data
+﻿using Newtonsoft.Json;
+
+namespace SharpRaven.Data
 {
     public class SentryUser
     {
+        [JsonProperty(PropertyName = "id", NullValueHandling = NullValueHandling.Ignore)]
+        public string Id { get; set; }
 
+        [JsonProperty(PropertyName = "username", NullValueHandling = NullValueHandling.Ignore)]
+        public string Username { get; set; }
+
+        [JsonProperty(PropertyName = "email", NullValueHandling = NullValueHandling.Ignore)]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "ip_address", NullValueHandling = NullValueHandling.Ignore)]
+        public string IpAddress { get; set; }
     }
 }
