1a54ff1ac33908aebaf89646872b8de71bafd3c3
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-03 17:42:54 +0100

    Started work to have the schema automatically generated

--- SharpRaven.UnitTests/SerializationTests.cs
+++ SharpRaven.UnitTests/SerializationTests.cs
@@ -4,6 +4,7 @@ using System.IO;
 
 using NUnit.Framework;
 
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
 
@@ -20,6 +21,11 @@ namespace SharpRaven.UnitTests
             int i = 10 / i2;
         }
 
+
+        private static string GetJsonSchemaPath()
+        {
+            return null;
+        }
 
         private static JsonSchema GetSchema()
         {
@@ -55,7 +61,51 @@ namespace SharpRaven.UnitTests
 
 
         [Test]
-        public void JsonPacket_ToString_ReturnsExpectedJsonString()
+        [Explicit]
+        public void GenerateJsonSchema()
+        {
+            JsonSchemaGenerator generator = new JsonSchemaGenerator();
+            var type = typeof (JsonPacket);
+            var schema = generator.Generate(type);
+            var path = Path.Combine(typeof (SerializationTests).Assembly.Location, "..", "..");
+            path = new DirectoryInfo(path).FullName;
+            Console.WriteLine(path);
+
+            using (var writer = new StringWriter())
+            using (var jsonTextWriter = new JsonTextWriter(writer))
+            {
+                schema.WriteTo(jsonTextWriter);
+            }
+        }
+
+
+        [Test]
+        public void JsonPacket_WithMissingLevel_IsNotValid()
+        {
+            var exception = GetException();
+
+            JsonPacket packet = new JsonPacket("https://public:secret@app.getsentry.com/1337", exception)
+            {
+                Tags = new Dictionary<string, string>
+                {
+                    { "key1", "value1" },
+                    { "key2", "value2" },
+                },
+            };
+
+            JObject jPacket = JObject.Parse(packet.ToString());
+            JsonSchema schema = GetSchema();
+
+            Console.WriteLine(jPacket);
+
+            var valid = jPacket.IsValid(schema);
+
+            Assert.That(valid, Is.False);
+        }
+
+
+        [Test]
+        public void JsonPacket_WithValidData_IsValid()
         {
             var exception = GetException();
 
