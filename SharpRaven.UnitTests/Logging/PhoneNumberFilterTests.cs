06cb85bcdc748d1f39eb8fb39167c976fb5db2bf
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-05 14:25:45 +0100

    Added a negative PhoneNumberFilter test and changed the test naming to not contain "Filter".

--- SharpRaven.UnitTests/Logging/PhoneNumberFilterTests.cs
+++ SharpRaven.UnitTests/Logging/PhoneNumberFilterTests.cs
@@ -10,7 +10,22 @@ namespace SharpRaven.UnitTests.Logging
     public class PhoneNumberFilterTests
     {
         [Test]
-        public void Filter_PhoneNumberIsScrubbed()
+        public void InvalidPhoneNumber_IsNotScrubbed()
+        {
+            const string phoneNumber = "1531";
+            var input = String.Format(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. {0} Praesent est dui, ornare eget condimentum a, tincidunt sit amet lectus. Nulla pellentesque, tortor eget tempus malesuada.",
+                phoneNumber);
+
+            PhoneNumberFilter filter = new PhoneNumberFilter();
+            var output = filter.Filter(input);
+
+            Assert.That(output, Is.StringContaining(phoneNumber));
+        }
+
+
+        [Test]
+        public void ValidPhoneNumber_IsScrubbed()
         {
             const string phoneNumber = "55518231234";
             var input = String.Format(
@@ -20,7 +35,7 @@ namespace SharpRaven.UnitTests.Logging
             PhoneNumberFilter filter = new PhoneNumberFilter();
             var output = filter.Filter(input);
 
-            Assert.That(output, Is.Not.Contains(phoneNumber));
+            Assert.That(output, Is.Not.StringContaining(phoneNumber));
         }
     }
 }
