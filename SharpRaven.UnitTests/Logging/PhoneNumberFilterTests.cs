5daea56fcffa67b4107b0b89805c6b97362f02be
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-05 14:11:39 +0100

    - Added failing test for the PhoneNumberFilter, proving it doesn't work because replacement isn't done in place, but returned as a new string instance. - Added failing CreditCardFilter test, unveiling a bug in the filter that leads to an IndexOutOfRangeException.

---
+++ SharpRaven.UnitTests/Logging/PhoneNumberFilterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using SharpRaven.Logging.Filters;
+
+namespace SharpRaven.UnitTests.Logging
+{
+    [TestFixture]
+    public class PhoneNumberFilterTests
+    {
+        [Test]
+        public void Filter_PhoneNumberIsScrubbed()
+        {
+            const string phoneNumber = "55518231234";
+            var input = String.Format(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. {0} Praesent est dui, ornare eget condimentum a, tincidunt sit amet lectus. Nulla pellentesque, tortor eget tempus malesuada.",
+                phoneNumber);
+
+            PhoneNumberFilter filter = new PhoneNumberFilter();
+            var output = filter.Filter(input);
+
+            Assert.That(output, Is.Not.Contains(phoneNumber));
+        }
+    }
+}
