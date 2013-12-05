3692f0a0e486f7f54a4960095a190adcba59f23e
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-05 14:19:35 +0100

    Changed existing CreditCardTest into a negative one due to it using an invalid credit card number and added a positive test for a valid credit card number

--- SharpRaven.UnitTests/Logging/CreditCardFilterTests.cs
+++ SharpRaven.UnitTests/Logging/CreditCardFilterTests.cs
@@ -10,7 +10,7 @@ namespace SharpRaven.UnitTests.Logging
     public class CreditCardFilterTests
     {
         [Test]
-        public void Filter_CreditCardNumberIsScrubbed()
+        public void Filter_InvalidCreditCardNumberIsNotScrubbed()
         {
             const string creditCardNumber = "1234-5678-9101-1121";
             var input = String.Format(
@@ -20,7 +20,22 @@ namespace SharpRaven.UnitTests.Logging
             CreditCardFilter filter = new CreditCardFilter();
             var output = filter.Filter(input);
 
-            Assert.That(output, Is.Not.Contains(creditCardNumber));
+            Assert.That(output, Is.StringContaining(creditCardNumber));
+        }
+
+
+        [Test]
+        public void Filter_ValidCreditCardNumberIsScrubbed()
+        {
+            const string creditCardNumber = "5271-1902-4264-3112";
+            var input = String.Format(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. {0} Praesent est dui, ornare eget condimentum a, tincidunt sit amet lectus. Nulla pellentesque, tortor eget tempus malesuada.",
+                creditCardNumber);
+
+            CreditCardFilter filter = new CreditCardFilter();
+            var output = filter.Filter(input);
+
+            Assert.That(output, Is.Not.StringContaining(creditCardNumber));
         }
     }
 }
