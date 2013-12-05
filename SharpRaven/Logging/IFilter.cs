f08df4cced1c0c68116f329fa724fca631179994
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-05 14:10:46 +0100

    Added documentation to SharpRaven.Logging.IFilter

--- SharpRaven/Logging/IFilter.cs
+++ SharpRaven/Logging/IFilter.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using SharpRaven.Data;
-
-namespace SharpRaven.Logging {
+﻿namespace SharpRaven.Logging {
     /// <summary>
     /// Interface for providing a 'filter' for log scrubbers.
     /// Used primarily for the default filter, but can be used
     /// by others if needed.
     /// </summary>
     public interface IFilter {
+        /// <summary>
+        /// Filters the specified input and returns the filtered <see cref="string"/>.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>
+        /// The filtered <see cref="string"/>.
+        /// </returns>
         string Filter(string input);
     }
 }
