02d3bcea8bed93603220f7941fbd5683e01508c1
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-05 13:57:21 +0100

    Added failing CreditCardFilter test, unveiling a bug in the filter that leads to an IndexOutOfRangeException.

---
+++ SharpRaven.UnitTests/Logging/CreditCardFilterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using SharpRaven.Logging.Filters;
+
+namespace SharpRaven.UnitTests.Logging
+{
+    [TestFixture]
+    public class CreditCardFilterTests
+    {
+        [Test]
+        public void Filter_CreditCardNumberIsScrubbed()
+        {
+            const string creditCardNumber = "1234-5678-9101-1121";
+            var input = String.Format(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. {0} Praesent est dui, ornare eget condimentum a, tincidunt sit amet lectus. Nulla pellentesque, tortor eget tempus malesuada.",
+                creditCardNumber);
+
+            CreditCardFilter filter = new CreditCardFilter();
+            var output = filter.Filter(input);
+
+            Assert.That(output, Is.Not.Contains(creditCardNumber));
+        }
+    }
+}
