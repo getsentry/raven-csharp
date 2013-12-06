dc7efb2bfe17ceb021eb18ec30733c2b71d1677c
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-06 10:22:30 +0100

    Refactored PhoneNumberFilterTests to use the abstract FilterTestsBase class.

--- SharpRaven.UnitTests/Logging/PhoneNumberFilterTests.cs
+++ SharpRaven.UnitTests/Logging/PhoneNumberFilterTests.cs
@@ -1,41 +1,23 @@
-﻿using System;
-
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 using SharpRaven.Logging.Filters;
 
 namespace SharpRaven.UnitTests.Logging
 {
     [TestFixture]
-    public class PhoneNumberFilterTests
+    public class PhoneNumberFilterTests : FilterTestsBase<PhoneNumberFilter>
     {
         [Test]
         public void InvalidPhoneNumber_IsNotScrubbed()
         {
-            const string phoneNumber = "1531";
-            var input = String.Format(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. {0} Praesent est dui, ornare eget condimentum a, tincidunt sit amet lectus. Nulla pellentesque, tortor eget tempus malesuada.",
-                phoneNumber);
-
-            PhoneNumberFilter filter = new PhoneNumberFilter();
-            var output = filter.Filter(input);
-
-            Assert.That(output, Is.StringContaining(phoneNumber));
+            base.InvalidValueIsNotScrubbed("1531");
         }
 
 
         [Test]
         public void ValidPhoneNumber_IsScrubbed()
         {
-            const string phoneNumber = "55518231234";
-            var input = String.Format(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. {0} Praesent est dui, ornare eget condimentum a, tincidunt sit amet lectus. Nulla pellentesque, tortor eget tempus malesuada.",
-                phoneNumber);
-
-            PhoneNumberFilter filter = new PhoneNumberFilter();
-            var output = filter.Filter(input);
-
-            Assert.That(output, Is.Not.StringContaining(phoneNumber));
+            ValidValueIsScrubbed("55518231234");
         }
     }
 }
