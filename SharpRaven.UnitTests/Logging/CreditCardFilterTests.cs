4b60f99d24d483e643c8628a3da8eda379175212
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-06 10:22:21 +0100

    Refactored CreditCardFilterTests to use the abstract FilterTestsBase class.

--- SharpRaven.UnitTests/Logging/CreditCardFilterTests.cs
+++ SharpRaven.UnitTests/Logging/CreditCardFilterTests.cs
@@ -1,41 +1,23 @@
-﻿using System;
-
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 using SharpRaven.Logging.Filters;
 
 namespace SharpRaven.UnitTests.Logging
 {
     [TestFixture]
-    public class CreditCardFilterTests
+    public class CreditCardFilterTests : FilterTestsBase<CreditCardFilter>
     {
         [Test]
         public void InvalidCreditCardNumber_IsNotScrubbed()
         {
-            const string creditCardNumber = "1234-5678-9101-1121";
-            var input = String.Format(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. {0} Praesent est dui, ornare eget condimentum a, tincidunt sit amet lectus. Nulla pellentesque, tortor eget tempus malesuada.",
-                creditCardNumber);
-
-            CreditCardFilter filter = new CreditCardFilter();
-            var output = filter.Filter(input);
-
-            Assert.That(output, Is.StringContaining(creditCardNumber));
+            InvalidValueIsNotScrubbed("1234-5678-9101-1121");
         }
 
 
         [Test]
         public void ValidCreditCardNumber_IsScrubbed()
         {
-            const string creditCardNumber = "5271-1902-4264-3112";
-            var input = String.Format(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. {0} Praesent est dui, ornare eget condimentum a, tincidunt sit amet lectus. Nulla pellentesque, tortor eget tempus malesuada.",
-                creditCardNumber);
-
-            CreditCardFilter filter = new CreditCardFilter();
-            var output = filter.Filter(input);
-
-            Assert.That(output, Is.Not.StringContaining(creditCardNumber));
+            ValidValueIsScrubbed("5271-1902-4264-3112");
         }
     }
 }
