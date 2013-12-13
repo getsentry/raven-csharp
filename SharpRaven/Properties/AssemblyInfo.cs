a2472421961ed20659a1c75af4ea81df4631db25
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-13 09:25:00 +0100

    Upped the version number to 1.0.0.0

--- SharpRaven/Properties/AssemblyInfo.cs
+++ SharpRaven/Properties/AssemblyInfo.cs
@@ -14,10 +14,10 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b5683941-1254-484e-b074-87cedd4fc78e")]
 
 // AssemblyVersion the most formal version number and is akin to "API Version". It doesn't need to change unless there's breaking changes.
-[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 // AssemblyFileVersion is more informal and can be increased more rapidly and with less consideration than AssemblyVersion. 
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // AssemblyInformationalVersion is even more informal than AssemblyFileVersion and doesn't need a certain format. It will be used as the $version$ replacement string in NuGet and can contain suffixes like "-alpha".
 // [assembly: AssemblyInformationalVersion("0.8.0.0")]
