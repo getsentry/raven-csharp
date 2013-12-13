65aa1b51e8a1c4c5a9a8164bad1d730daa180d77
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-13 16:51:56 +0100

    Updated test so it validates the correct data.

--- SharpRaven.UnitTests/Utilities/PacketBuilderTests.cs
+++ SharpRaven.UnitTests/Utilities/PacketBuilderTests.cs
@@ -11,14 +11,14 @@ namespace SharpRaven.UnitTests.Utilities
         public void CreateAuthenticationHeader_ReturnsCorrectAuthenticationHeader()
         {
             const string expected =
-                @"^Sentry sentry_version=4, sentry_client=SharpRaven/1.0, sentry_timestamp=\d+, sentry_key=7d6466e66155431495bdb4036ba9a04b, sentry_secret=4c1cfeab7ebd4c1cb9e18008173a3630$";
+                @"^Sentry sentry_version=4, sentry_client=SharpRaven/[\d\.]+, sentry_timestamp=\d+, sentry_key=7d6466e66155431495bdb4036ba9a04b, sentry_secret=4c1cfeab7ebd4c1cb9e18008173a3630$";
 
             var dsn = new Dsn(
                 "https://7d6466e66155431495bdb4036ba9a04b:4c1cfeab7ebd4c1cb9e18008173a3630@app.getsentry.com/3739");
 
             var authenticationHeader = PacketBuilder.CreateAuthenticationHeader(dsn);
 
-            Assert.That(authenticationHeader, Is.StringMatching(""));
+            Assert.That(authenticationHeader, Is.StringMatching(expected));
         }
     }
 }
