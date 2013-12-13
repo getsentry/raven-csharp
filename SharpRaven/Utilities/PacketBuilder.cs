a60d48fc69c0c4d33d6a3cbea26331b5080aebbf
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-13 16:49:55 +0100

    Rewrote PacketBuilder.CreateAuthenticationHeader so it uses String.Format instead of string concatenation and added a dynamically created UserAgent property so the version number can be fetched from assembly metadata instead of being hard coded.

--- SharpRaven/Utilities/PacketBuilder.cs
+++ SharpRaven/Utilities/PacketBuilder.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace SharpRaven.Utilities {
+namespace SharpRaven.Utilities
+{
     /// <summary>
     /// Utility class for building 
     /// </summary>
-    public static class PacketBuilder {
+    public static class PacketBuilder
+    {
+        private static string userAgent;
+
+
+        static PacketBuilder()
+        {
+            var assemblyName = typeof(PacketBuilder).Assembly.GetName();
+            var name = assemblyName.Name;
+            var version = assemblyName.Version;
+            userAgent = String.Format("{0}/{1}", name, version);
+        }
+
+
+        /// <summary>
+        /// Gets the user agent string for Sentry.
+        /// </summary>
+        /// <value>
+        /// The user agent string for Sentry.
+        /// </value>
+        public static string UserAgent
+        {
+            get { return userAgent; }
+            private set { userAgent = value; }
+        }
+
+
         /// <summary>
         /// Creates the authentication header base on the provided <see cref="Dsn"/>.
         /// </summary>
@@ -14,15 +39,17 @@ namespace SharpRaven.Utilities {
         /// <returns>
         /// The authentication header.
         /// </returns>
-        public static string CreateAuthenticationHeader(Dsn dsn) {
-            string header = String.Empty;
-            header += "Sentry sentry_version=4";
-            header += ", sentry_client=SharpRaven/1.0";
-            header += ", sentry_timestamp=" + (long)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
-            header += ", sentry_key=" + dsn.PublicKey;
-            header += ", sentry_secret=" + dsn.PrivateKey;
-
-            return header;
+        public static string CreateAuthenticationHeader(Dsn dsn)
+        {
+            return String.Format("Sentry sentry_version=4"
+                                 + ", sentry_client={0}"
+                                 + ", sentry_timestamp={1}"
+                                 + ", sentry_key={2}"
+                                 + ", sentry_secret={3}",
+                                 UserAgent,
+                                 (long) (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds,
+                                 dsn.PublicKey,
+                                 dsn.PrivateKey);
         }
     }
 }
