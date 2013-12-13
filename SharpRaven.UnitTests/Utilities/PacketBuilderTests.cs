ae4bb5ad5632663b985e5233257d47f58776c407
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-13 16:49:05 +0100

    Added unit test for PacketBuilder.CreateAuthenticationHeader

---
+++ SharpRaven.UnitTests/Utilities/PacketBuilderTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+using SharpRaven.Utilities;
+
+namespace SharpRaven.UnitTests.Utilities
+{
+    [TestFixture]
+    public class PacketBuilderTests
+    {
+        [Test]
+        public void CreateAuthenticationHeader_ReturnsCorrectAuthenticationHeader()
+        {
+            const string expected =
+                @"^Sentry sentry_version=4, sentry_client=SharpRaven/1.0, sentry_timestamp=\d+, sentry_key=7d6466e66155431495bdb4036ba9a04b, sentry_secret=4c1cfeab7ebd4c1cb9e18008173a3630$";
+
+            var dsn = new Dsn(
+                "https://7d6466e66155431495bdb4036ba9a04b:4c1cfeab7ebd4c1cb9e18008173a3630@app.getsentry.com/3739");
+
+            var authenticationHeader = PacketBuilder.CreateAuthenticationHeader(dsn);
+
+            Assert.That(authenticationHeader, Is.StringMatching(""));
+        }
+    }
+}
