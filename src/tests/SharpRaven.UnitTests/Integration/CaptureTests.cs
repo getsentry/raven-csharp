c9165dbefb6f9cdf4f6fafe2adede75a8c9c4d1c
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-12-17 15:03:31 +0100

    CaptureTests should use the IRavenClient interface instead of the concrete class to get test coverage for the interface as well.

--- src/tests/SharpRaven.UnitTests/Integration/CaptureTests.cs
+++ src/tests/SharpRaven.UnitTests/Integration/CaptureTests.cs
@@ -61,7 +61,7 @@ namespace SharpRaven.UnitTests.Integration
         private const string DsnUrl =
             "https://7d6466e66155431495bdb4036ba9a04b:4c1cfeab7ebd4c1cb9e18008173a3630@app.getsentry.com/3739";
 
-        private RavenClient ravenClient;
+        private IRavenClient ravenClient;
 
 
         private static void SecondLevelException()
