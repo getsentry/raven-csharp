2b08830ec545a9288a3e797c1b38630b0d205613
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2014-11-10 14:05:05 +0100

    Output headers in the build file to increase readability of the command line output.

--- src/Build.cmd
+++ src/Build.cmd
@@ -1,7 +1,27 @@
 @echo off
 SETLOCAL
 SET PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319;%CD%\..\.nuget;
+echo.
+echo.
+echo ==================
+echo === GitVersion ===
+echo ==================
+echo.
 packages\GitVersion.CommandLine.1.3.3\Tools\GitVersion.exe /output json /updateassemblyinfo true /l gitversion.log
+echo.
+echo.
+echo.
+echo.
+echo ==================
+echo ===  MSBuild   ===
+echo ==================
+echo.
 MSBUILD SharpRaven.build
+echo.
+echo ==================
+echo ===   NuGet    ===
+echo ==================
+echo.
 NuGet pack app\SharpRaven\SharpRaven.nuspec -Properties ReleaseInfo='Test'
+echo.
 ENDLOCAL
