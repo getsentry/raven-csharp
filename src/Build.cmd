07d5589454ab94c14bfb49dd895dc97790d84dbb
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2014-11-11 09:49:57 +0100

    Do NuGet Restore in the build script

--- src/Build.cmd
+++ src/Build.cmd
@@ -3,24 +3,31 @@ SETLOCAL
 SET PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319;%CD%\..\.nuget;
 echo.
 echo.
-echo ==================
-echo === GitVersion ===
-echo ==================
+echo =====================
+echo === NuGet Restore ===
+echo =====================
+echo.
+NuGet restore SharpRaven.sln
+echo.
+echo.
+echo =====================
+echo ===   GitVersion  ===
+echo =====================
 echo.
 packages\GitVersion.CommandLine.1.3.3\Tools\GitVersion.exe /output json /updateassemblyinfo true /l gitversion.log
 echo.
 echo.
 echo.
 echo.
-echo ==================
-echo ===  MSBuild   ===
-echo ==================
+echo =====================
+echo ===    MSBuild    ===
+echo =====================
 echo.
 MSBUILD SharpRaven.build
 echo.
-echo ==================
-echo ===   NuGet    ===
-echo ==================
+echo =====================
+echo ===  NuGet Pack   ===
+echo =====================
 echo.
 NuGet pack app\SharpRaven\SharpRaven.nuspec -Properties ReleaseInfo='Test'
 echo.
