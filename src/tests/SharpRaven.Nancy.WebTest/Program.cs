d798938b168c84fe5513f391d03fa572fff794aa
Patrizio Bertozzi <xpicio@gmail.com>
2014-11-21 09:58:12 +0100

    Add new web test project SharpRaven.Nancy.WebTest

---
+++ src/tests/SharpRaven.Nancy.WebTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpRaven.Nancy.WebTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:3579")))
+            {
+                host.Start();
+
+                Console.WriteLine("Nancy is up and runnig. Press any key to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
