8c7e0d4c568ec47394c5555b2d9a87efbb4c6dad
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2014-11-24 15:31:58 +0100

    Rejigged the DefaultModule to throw exception so the ApplicationStartup pipeline registration can act upon it.

--- src/tests/SharpRaven.Nancy.WebTest/DefaultModule.cs
+++ src/tests/SharpRaven.Nancy.WebTest/DefaultModule.cs
@@ -1,15 +1,51 @@
-﻿using Nancy;
-using Nancy.Hosting.Self;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region License
+
+// Copyright (c) 2014 The Sentry Team and individual contributors.
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+// 
+//     1. Redistributions of source code must retain the above copyright notice, this list of
+//        conditions and the following disclaimer.
+// 
+//     2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//        conditions and the following disclaimer in the documentation and/or other materials
+//        provided with the distribution.
+// 
+//     3. Neither the name of the Sentry nor the names of its contributors may be used to
+//        endorse or promote products derived from this software without specific prior written
+//        permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#endregion
+
+using Nancy;
 
 namespace SharpRaven.Nancy.WebTest
 {
     public class DefaultModule : NancyModule
     {
+        public DefaultModule()
+        {
+            Get["/"] = _ => View["default.html"];
+
+            Post["/"] = _ =>
+            {
+                DivideByZero();
+                return View["error.html"];
+            };
+        }
+
+
         private static void DivideByZero(int stackFrames = 10)
         {
             if (stackFrames == 0)
@@ -21,30 +57,6 @@ namespace SharpRaven.Nancy.WebTest
             {
                 DivideByZero(--stackFrames);
             }
-        }
-
-        public DefaultModule()
-        {
-            Get["/"] = _ => {
-                return View["default.html"];
-            };
-
-            Post["/"] = _ =>
-            {
-                string exceptionId = String.Empty;
-
-                try
-                {
-                    DivideByZero();
-                }
-                catch (Exception ex)
-                {
-                    IRavenClient ravenClient = new RavenClient(this.Context);
-                    exceptionId = ravenClient.CaptureException(ex);
-                }
-
-                return View["error.html", new { ExceptionId = exceptionId }];
-            };
         }
     }
 }
