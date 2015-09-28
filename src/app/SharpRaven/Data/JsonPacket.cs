dc07747669e1791c3d23710d716676514e70dd05
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-09-28 12:06:45 +0200

    R#

--- src/app/SharpRaven/Data/JsonPacket.cs
+++ src/app/SharpRaven/Data/JsonPacket.cs
@@ -2,21 +2,21 @@
 
 // Copyright (c) 2014 The Sentry Team and individual contributors.
 // All rights reserved.
-//
+// 
 // Redistribution and use in source and binary forms, with or without modification, are permitted
 // provided that the following conditions are met:
-//
+// 
 //     1. Redistributions of source code must retain the above copyright notice, this list of
 //        conditions and the following disclaimer.
-//
+// 
 //     2. Redistributions in binary form must reproduce the above copyright notice, this list of
 //        conditions and the following disclaimer in the documentation and/or other materials
 //        provided with the distribution.
-//
+// 
 //     3. Neither the name of the Sentry nor the names of its contributors may be used to
 //        endorse or promote products derived from this software without specific prior written
 //        permission.
-//
+// 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
 // IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 // FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
@@ -226,6 +226,12 @@ namespace SharpRaven.Data
         public string Project { get; set; }
 
         /// <summary>
+        /// Identifies the version of the application.
+        /// </summary>
+        [JsonProperty(PropertyName = "release", NullValueHandling = NullValueHandling.Ignore)]
+        public string Release { get; set; }
+
+        /// <summary>
         /// Gets or sets the <see cref="SentryRequest"/> object, containing information about the HTTP request.
         /// </summary>
         /// <value>
@@ -239,12 +245,6 @@ namespace SharpRaven.Data
         /// </summary>
         [JsonProperty(PropertyName = "server_name", NullValueHandling = NullValueHandling.Ignore)]
         public string ServerName { get; set; }
-
-        /// <summary>
-        /// Identifies the version of the application.
-        /// </summary>
-        [JsonProperty(PropertyName = "release", NullValueHandling = NullValueHandling.Ignore)]
-        public string Release { get; set; }
 
         /// <summary>
         /// A map or list of tags for this event.
