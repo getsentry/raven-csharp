014abe80ac131324aa92d31ff2875ede98297125
Matt Robenolt <matt@ydekproductions.com>
2015-10-09 14:08:26 -0700

    Add fingerprint to interface

--- src/app/SharpRaven/IRavenClient.cs
+++ src/app/SharpRaven/IRavenClient.cs
@@ -2,21 +2,21 @@
 
 // Copyright (c) 2014 The Sentry Team and individual contributors.
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification, are permitted
 // provided that the following conditions are met:
-// 
+//
 //     1. Redistributions of source code must retain the above copyright notice, this list of
 //        conditions and the following disclaimer.
-// 
+//
 //     2. Redistributions in binary form must reproduce the above copyright notice, this list of
 //        conditions and the following disclaimer in the documentation and/or other materials
 //        provided with the distribution.
-// 
+//
 //     3. Neither the name of the Sentry nor the names of its contributors may be used to
 //        endorse or promote products derived from this software without specific prior written
 //        permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
 // IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 // FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
@@ -58,14 +58,14 @@ namespace SharpRaven
         string Logger { get; set; }
 
         /// <summary>
-        /// Interface for providing a 'log scrubber' that removes 
+        /// Interface for providing a 'log scrubber' that removes
         /// sensitive information from exceptions sent to sentry.
         /// </summary>
         IScrubber LogScrubber { get; set; }
 
         /// <summary>
         /// Gets or sets the timeout value in milliseconds for the <see cref="System.Net.HttpWebRequest.GetResponse()"/>
-        /// and <see cref="System.Net.HttpWebRequest.GetRequestStream()"/> methods. 
+        /// and <see cref="System.Net.HttpWebRequest.GetRequestStream()"/> methods.
         /// </summary>
         /// <value>
         /// The number of milliseconds to wait before the request times out. The default is 5,000 milliseconds (5 seconds).
@@ -81,6 +81,7 @@ namespace SharpRaven
         /// <param name="message">The optional messge to capture instead of the default <see cref="Exception.Message" />.</param>
         /// <param name="level">The <see cref="ErrorLevel" /> of the captured <paramref name="exception" />. Default: <see cref="ErrorLevel.Error" />.</param>
         /// <param name="tags">The tags to annotate the captured <paramref name="exception" /> with.</param>
+        /// <param name="fingerprint">The custom fingerprint to annotate the captured <paramref name="message" /> with.</param>
         /// <param name="extra">The extra metadata to send with the captured <paramref name="exception" />.</param>
         /// <returns>
         /// The <see cref="JsonPacket.EventID" /> of the successfully captured <paramref name="exception" />, or <c>null</c> if it fails.
@@ -89,6 +90,7 @@ namespace SharpRaven
                                 SentryMessage message = null,
                                 ErrorLevel level = ErrorLevel.Error,
                                 IDictionary<string, string> tags = null,
+                                List<string> fingerprint = null,
                                 object extra = null);
 
 
@@ -118,6 +120,7 @@ namespace SharpRaven
         /// <param name="message">The message to capture.</param>
         /// <param name="level">The <see cref="ErrorLevel" /> of the captured <paramref name="message" />. Default <see cref="ErrorLevel.Info" />.</param>
         /// <param name="tags">The tags to annotate the captured <paramref name="message" /> with.</param>
+        /// <param name="fingerprint">The custom fingerprint to annotate the captured <paramref name="message" /> with.</param>
         /// <param name="extra">The extra metadata to send with the captured <paramref name="message" />.</param>
         /// <returns>
         /// The <see cref="JsonPacket.EventID" /> of the successfully captured <paramref name="message" />, or <c>null</c> if it fails.
@@ -125,6 +128,7 @@ namespace SharpRaven
         string CaptureMessage(SentryMessage message,
                               ErrorLevel level = ErrorLevel.Info,
                               Dictionary<string, string> tags = null,
+                              List<string> fingerprint = null,
                               object extra = null);
 
         #region Deprecated Methods
