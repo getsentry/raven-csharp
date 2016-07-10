be8dbcbfbd73d09d207ad8a1d4673a815e0e7570
Samuel Pereira <samuel.p.araujo@gmail.com>
2016-07-10 20:43:20 -0300

    Refactor to a thread-safe queue.

--- src/app/SharpRaven/Utilities/CircularBuffer.cs
+++ src/app/SharpRaven/Utilities/CircularBuffer.cs
@@ -1,31 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SharpRaven.Utilities {
     public class CircularBuffer<T>
     {
         private readonly int size;
-        private readonly Queue<T> queue;
+        private ConcurrentQueue<T> queue;
 
         public CircularBuffer(int size = 100)
         {
             this.size = size;
-            queue = new Queue<T>();
+            queue = new ConcurrentQueue<T>();
         }
 
         public List<T> ToList()
         {
-            return queue.ToList();
+            var listReturn = this.queue.ToList();
+            
+            return listReturn.Skip(Math.Max(0, listReturn.Count - size)).ToList();
         }
 
         public void Clear()
         {
-            queue.Clear();
+            queue = new ConcurrentQueue<T>();
         }
 
         public void Add(T item) {
             if (queue.Count >= size)
-                queue.Dequeue();
+            {
+                T result;
+                this.queue.TryDequeue(out result);
+            }
 
             queue.Enqueue(item);
         }
